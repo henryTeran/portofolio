274c1cda0f40900ed2b6f08ebdd5208569911ce0
Full Stack developer <65180848+henryTeran@users.noreply.github.com>
2025-09-30 15:48:58 +0200

    Updated emailService.ts

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,7 +1,7 @@
 import emailjs from '@emailjs/browser';
 
 // Configuration EmailJS
-const EMAILJS_SERVICE_ID = 'service_portfolio'; // À configurer dans EmailJS
+const EMAILJS_SERVICE_ID = 'portofolio_HenryTeran'; // À configurer dans EmailJS
 const EMAILJS_TEMPLATE_ID = 'template_contact'; // Template pour contact simple
 const EMAILJS_QUOTE_TEMPLATE_ID = 'template_quote'; // Template pour demande de devis
 const EMAILJS_PUBLIC_KEY = 'xZiaWwWG4X44LV4sB'; // À configurer dans EmailJS
